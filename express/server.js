5d4f4bd5d04ee8f475970f7ab6fa5332ba8ce9cd
janarthan <janarthan@finnovation.lk>
2017-10-08 14:08:49 +0530

    Update

--- express/server.js
+++ express/server.js
@@ -4,3 +4,7 @@ var app = express();
 var server = app.listen(3000, function () {
     console.log("Server is up and running on 3000");
 });
+
+app.get('/', function (req, res) {
+    res.send("hello from mighty express");
+});
