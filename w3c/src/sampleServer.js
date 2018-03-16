df00f651633829c52d7ce28b1285e630569fdc79
janarthan <janarthan@finnovation.lk>
2018-03-16 10:03:27 +0530

    import module

--- w3c/src/sampleServer.js
+++ w3c/src/sampleServer.js
@@ -1,6 +1,7 @@
 let http = require('http');
+let clock = require('./myFirstModule');
 
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type':'text/html'});
-   res.end('Hello World!');
-}).listen(8080);
+   res.end('The date and time : ' + clock.myDateTime());
+}).listen(8888);
