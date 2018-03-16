0501c363d125e318cf268118a0354d880e7800f8
janarthan <janarthan@finnovation.lk>
2018-03-16 10:18:13 +0530

    Read from file

--- w3c/src/sampleServer.js
+++ w3c/src/sampleServer.js
@@ -1,9 +1,18 @@
 let http = require('http');
 let clock = require('./myFirstModule');
+let fs = require('fs');
 
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type':'text/html'});
    res.write('The date and time : ' + clock.myDateTime());
    res.write("\n url:" + req.url);
    res.end();
+}).listen(8877);
+
+http.createServer(function (req, res) {
+   fs.readFile('./../other/profile.txt', function (err, data) {
+       res.writeHead(200, {'Content-Type':'text/html'});
+       res.write(data);
+       res.end();
+   });
 }).listen(8888);
