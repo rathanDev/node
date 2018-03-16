b55f306d7e533903064d6fde88e2954c816cf302
janarthan <janarthan@finnovation.lk>
2018-03-16 10:08:57 +0530

    Use request.url

--- w3c/src/sampleServer.js
+++ w3c/src/sampleServer.js
@@ -4,5 +4,6 @@ let clock = require('./myFirstModule');
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type':'text/html'});
    res.write('The date and time : ' + clock.myDateTime());
+   res.write("\n url:" + req.url);
    res.end();
 }).listen(8888);
