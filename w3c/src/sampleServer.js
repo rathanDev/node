643b638977da25d6d62c0520362dbad6d5deaeb2
janarthan <janarthan@finnovation.lk>
2018-03-16 10:07:04 +0530

    Refactor

--- w3c/src/sampleServer.js
+++ w3c/src/sampleServer.js
@@ -3,5 +3,6 @@ let clock = require('./myFirstModule');
 
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type':'text/html'});
-   res.end('The date and time : ' + clock.myDateTime());
+   res.write('The date and time : ' + clock.myDateTime());
+   res.end();
 }).listen(8888);
