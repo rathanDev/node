7603684c1f73961de78e22dd23d1fb39b54fd8ec
janarthan <janarthan@finnovation.lk>
2018-03-16 10:20:35 +0530

    Handle err

--- w3c/src/sampleServer.js
+++ w3c/src/sampleServer.js
@@ -3,16 +3,21 @@ let clock = require('./myFirstModule');
 let fs = require('fs');
 
 http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type':'text/html'});
-   res.write('The date and time : ' + clock.myDateTime());
-   res.write("\n url:" + req.url);
-   res.end();
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('The date and time : ' + clock.myDateTime());
+    res.write("\n url:" + req.url);
+    res.end();
 }).listen(8877);
 
 http.createServer(function (req, res) {
-   fs.readFile('./../other/profile.txt', function (err, data) {
-       res.writeHead(200, {'Content-Type':'text/html'});
-       res.write(data);
-       res.end();
-   });
+
+    fs.readFile('../other/profile.txt', function (err, data) {
+        if (err) {
+            throw err;
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+
 }).listen(8888);
