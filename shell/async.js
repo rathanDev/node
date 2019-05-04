1fae1c04766df1c646c98b516f1a09730231c257
jana <rathan.kparam@gmail.com>
2019-05-04 18:46:46 +0530

    Update

--- shell/async.js
+++ shell/async.js
@@ -5,16 +5,21 @@ async function ls() {
 
     const { stdout, stderr } = await exec(`aws s3 ls --recursive s3://dev.trivyol.com --page-size 100 --profile triv-s3 | awk '{print $4,$5}'`);
 
-    console.log('stdout:', stdout);
-    console.log('stdout:', stdout.length);
+    // console.log('stdout:', stdout);
+    // console.log('stdout:', stdout.length);
 
     names = stdout.split("\n");
     console.log('names ', names);
     console.log('names len ', names.length);
 
-    // for(i=0; i=names.length; i++) {
-    //     console.log('i-- ', names[i]);
-    // }
+    for (var i = 0; i < names.length; i++) {
+        const name = names[i];
+        if(!name) {
+            console.log('Invalid ', name);
+            continue;
+        }
+        console.log('Name --------> ', name);
+    }
 
     console.log('stderr:', stderr);
 
