a2809d89deae43a397a0ae3545c964f4f32f5d5c
jana <rathan.kparam@gmail.com>
2019-05-04 19:22:55 +0530

    Update

--- shell/async.js
+++ shell/async.js
@@ -1,27 +1,35 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-
+// --page-size 100
 async function ls() {
 
-    const { stdout, stderr } = await exec(`aws s3 ls --recursive s3://dev.trivyol.com --page-size 100 --profile triv-s3 | awk '{print $4,$5}'`);
+    console.log('start 200', new Date()); // 19.11
+
+    const {stdout, stderr} = await exec(
+        `aws s3 ls --recursive s3://dev.trivyol.com --page-size 100 --profile triv-s3 | awk '{print $4,$5}'`,
+        {maxBuffer: 200 * 1024 * 1024}
+    );
 
     // console.log('stdout:', stdout);
     // console.log('stdout:', stdout.length);
 
+    console.log('got stdout');
+
     names = stdout.split("\n");
-    console.log('names ', names);
+    console.log('names med ', names);
     console.log('names len ', names.length);
 
-    for (var i = 0; i < names.length; i++) {
-        const name = names[i];
-        if(!name) {
-            console.log('Invalid ', name);
-            continue;
-        }
-        console.log('Name --------> ', name);
-    }
+    // for (var i = 0; i < names.length; i++) {
+    //     const name = names[i];
+    //     if(!name) {
+    //         console.log('Invalid ', name);
+    //         continue;
+    //     }
+    //     console.log('Name --------> ', name);
+    // }
 
     console.log('stderr:', stderr);
 
 }
+
 ls();
