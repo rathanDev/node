d1357dcaeaee155dfadcff40eb32ffb158f58dda
jana <rathan.kparam@gmail.com>
2019-05-04 22:10:59 +0530

    Refactor

--- shell/updateMetaData.js
+++ shell/updateMetaData.js
@@ -6,9 +6,7 @@ const maxIterations = 10;
 const maxItem = 10;
 const bucketName = 'dev.trivyol.com';
 
-async function ls() {
-
-    console.log('Start  ', new Date());
+async function update() {
 
     const {stdout: stdOut1, stdErr} = await exec(
         `aws s3api list-objects-v2 --bucket ${bucketName} --max-item 1 --profile triv-s3`,
@@ -50,8 +48,7 @@ async function ls() {
         for (var j = 0; j < files.length; j++) {
             const file = files[j];
             const name = file.Key;
-            console.log('File ', count, name);
-
+            console.log('File:'+count, name);
 
             const {stdout: stdOut2, stderr} = await exec(
                 `aws s3api put-object --bucket ${bucketName} --key ${name} --content-disposition attachment --content-type image/jpeg --profile triv-s3;`
@@ -65,15 +62,18 @@ async function ls() {
             const res = JSON.parse(stdOut2);
             console.log('Updated? ', res, '\n');
 
-
             count++;
         }
 
+        console.log('No of files : ', count);
+        console.log('End    ', new Date());
     }
-
-    console.log('No of files : ', count);
-    console.log('End    ', new Date());
-
 }
 
-ls();
+function main() {
+    console.log('Start processing ', new Date());
+
+    update();
+}
+
+main();
