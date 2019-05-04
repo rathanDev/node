9446ba74e62590f380b598d42d23f46f24b10b30
jana <rathan.kparam@gmail.com>
2019-05-04 22:26:27 +0530

    Refactor

--- shell/updateMetaData.js
+++ shell/updateMetaData.js
@@ -6,24 +6,12 @@ const maxIterations = 10;
 const maxItem = 10;
 const bucketName = 'dev.trivyol.com';
 
-async function update() {
-
-    const {stdout: stdOut1, stdErr} = await exec(
-        `aws s3api list-objects-v2 --bucket ${bucketName} --max-item 1 --profile triv-s3`,
-        {maxBuffer: 200 * 1024 * 1024}
-    );
-
-    if (stdErr) {
-        console.log('Err', stdErr);
-        return;
-    }
-
-    const res = JSON.parse(stdOut1);
-    var nextToken = res.NextToken;
+async function update(nextToken) {
+    console.log("Update");
 
     for (var i = 0; i < maxIterations; i++) {
 
-        if(!nextToken) {
+        if (!nextToken) {
             console.log('End of bucket');
             break;
         }
@@ -40,15 +28,12 @@ async function update() {
 
         const res = JSON.parse(stdOut2);
 
-        nextToken = res.NextToken;
-        console.log('nextToken', nextToken, '\n');
-
         const files = res.Contents;
 
         for (var j = 0; j < files.length; j++) {
             const file = files[j];
             const name = file.Key;
-            console.log('File:'+count, name);
+            console.log('File:' + count, name);
 
             const {stdout: stdOut2, stderr} = await exec(
                 `aws s3api put-object --bucket ${bucketName} --key ${name} --content-disposition attachment --content-type image/jpeg --profile triv-s3;`
@@ -65,15 +50,45 @@ async function update() {
             count++;
         }
 
-        console.log('No of files : ', count);
-        console.log('End    ', new Date());
+        nextToken = res.NextToken;
+        console.log('nextToken', nextToken, '\n');
+
     }
+
+    console.log('No of files : ', count);
+    console.log('End    ', new Date());
+}
+
+async function getFirstToken() {
+
+    // optional;
+    //return "eyJDb250aW51YXRpb25Ub2tlbiI6IG51bGwsICJib3RvX3RydW5jYXRlX2Ftb3VudCI6IDIxfQ==";
+
+    console.log('Get first token');
+
+    const {stdout: stdOut1, stdErr} = await exec(
+        `aws s3api list-objects-v2 --bucket ${bucketName} --max-item 1 --profile triv-s3`,
+        {maxBuffer: 200 * 1024 * 1024}
+    );
+    if (stdErr) {
+        console.log('Err', stdErr);
+        return;
+    }
+
+    const firstToken = JSON.parse(stdOut1).NextToken;
+    console.log('First token ', firstToken);
+
+    return firstToken;
 }
 
 function main() {
     console.log('Start processing ', new Date());
 
-    update();
+    getFirstToken()
+        .then(function (token) {
+            update(token);
+        });
+
 }
 
 main();
