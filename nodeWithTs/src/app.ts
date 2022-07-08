74f499ecf946f3c1394c1dc0f2e2ae31edb39b67
jana <rathan.kparam@gmail.com>
2022-07-08 20:11:53 +0800

    Update

--- nodeWithTs/src/app.ts
+++ nodeWithTs/src/app.ts
@@ -1,17 +1,19 @@
-console.log("Hellow NodeJs devs");
-
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
-app.use(express.json());        // middleware
+app.use(express.json()); // middleware
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   return res.send("Hello world");
 });
 
-app.post("/api/data", (req, res) => {
+app.post("/api/data", (req: Request, res: Response) => {
   console.log(req.body);
   return res.sendStatus(200);
+});
+
+app.all("/api/all", (req: Request, res: Response) => {
+  return res.sendStatus(300);
 });
 
 app.listen(3000, () => {
