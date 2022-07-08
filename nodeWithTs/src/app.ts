44c20af34b6afe0bfc8adc71eabd7e8ba84447f3
jana <rathan.kparam@gmail.com>
2022-07-08 20:24:26 +0800

    Do API chaining with route

--- nodeWithTs/src/app.ts
+++ nodeWithTs/src/app.ts
@@ -3,14 +3,29 @@ import express, { Request, Response } from "express";
 const app = express();
 app.use(express.json()); // middleware
 
-app.get("/", (req: Request, res: Response) => {
-  // return res.send("Hello world");
-  //   return res.json({
-  //     success: true,
-  //     name: "Jana"
-  //   });
-  return res.redirect("http://example.com");
-});
+// app.get("/", (req: Request, res: Response) => {
+//   // return res.send("Hello world");
+//   //   return res.json({
+//   //     success: true,
+//   //     name: "Jana"
+//   //   });
+//   return res.redirect("http://example.com");
+// });
+
+app
+  .route("/")
+  .get((req: Request, res: Response) => {
+    return res.send("A GET req");
+  })
+  .post((req: Request, res: Response) => {
+    return res.send("A POST req");
+  })
+  .put((req: Request, res: Response) => {
+    return res.send("A PUT req");
+  })
+  .all((req: Request, res: Response) => {
+    return res.send("An X req");
+  });
 
 app.post("/api/data", (req: Request, res: Response) => {
   console.log(req.body);
