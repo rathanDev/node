9fec950418ac5dc1369bba406c53593c676c4d87
jana <rathan.kparam@gmail.com>
2022-07-08 08:39:21 +0800

    Add an api for /

--- nodeWithTs/src/app.ts
+++ nodeWithTs/src/app.ts
@@ -1,1 +1,13 @@
 console.log("Hellow NodeJs devs");
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Hello world");
+});
+
+app.listen(3000, () => {
+  console.log("App listening at 3000");
+});
