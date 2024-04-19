71f0583e31109e2a349e92d3923112beb07483ee
jana <rathan.kparam@gmail.com>
2024-04-19 23:56:58 +0800

    Got it working now

--- node-ts-express-mongo/src/router/authentication.ts
+++ node-ts-express-mongo/src/router/authentication.ts
@@ -1,7 +1,11 @@
 import express from "express";
-// import { register } from "../controllers/authentication";
+import { register } from "../controllers/authentication";
 import * as simple from "../controllers/simple";
 
+// export default (router: express.Router) => {
+//   router.post("/auth/register", simple.testSimpleApi);
+// };
+
 export default (router: express.Router) => {
-  router.post("/auth/register", simple.testSimpleApi);
+  router.post("/auth/register", register);
 };
